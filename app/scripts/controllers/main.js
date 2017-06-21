36332e4da7b01f3c7400e75a46bd8ef104a4f82e
Sara <sarapearce3.14@gmail.com>
2017-06-21 10:16:46 -0500

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name crawlerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the crawlerApp
+ */
+angular.module('crawlerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
